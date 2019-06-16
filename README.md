7c7c52b5760e4ed102be8eaa61f22b10b93ccd20
toporek3112 <toporkarol@gmail.com>
2019-06-16 16:14:29 +0200

    the first commit

---
+++ README.md
@@ -0,0 +1,275 @@
+# WebUntis
+The purpose of this node.js module is to make the communication with the Webuntis API easier
+
+## Contents
+* [installation](https://github.com/toporek3112/node-webuntis#Istllation)
+* [Overall](https://github.com/toporek3112/node-webuntis#Overall)
+* [Methods](https://github.com/toporek3112/node-webuntis#Methods)
+    * [getSession](https://github.com/toporek3112/node-webuntis#getSession)
+    * [getTimetable](https://github.com/toporek3112/node-webuntis#getTimetable)
+    * [getTeachers](https://github.com/toporek3112/node-webuntis#getTeachers)
+    * [getStudents](https://github.com/toporek3112/node-webuntis#getStudents)
+    * [getSubjects](https://github.com/toporek3112/node-webuntis#getSubjects)
+    * [getRooms](https://github.com/toporek3112/node-webuntis#getRooms)
+    * [getDepartments](https://github.com/toporek3112/node-webuntis#getDepartments)
+    * [getHolidays](https://github.com/toporek3112/node-webuntis#getHolidays)
+    * [getTimeGrid](https://github.com/toporek3112/node-webuntis#getTimeGrid)
+    * [getCurrentSchoolYear](https://github.com/toporek3112/node-webuntis#getCurrentSchoolYear)
+    * [Logout](https://github.com/toporek3112/node-webuntis#Logout)
+
+
+### Istllation
+````
+npm install node-webuntis 
+````
+
+### Overall
+<table>
+<tr><th>personTypes </th>
+<tr><td>
+    
+id| type
+---|-----
+1|class
+2|teacher
+3|subject
+4|room
+5|student
+ </table>
+
+# Methods
+All methods are working with **promises**. In case you do not know how promises work, go and make yourself familiar with them (they very handy). 
+
+## getSession
+#### Parameters
+* schoolname
+* username 
+* password
+#### Usage
+````js
+let session = await getSession(schoolname, username, password)
+````
+#### Returns
+a Object containing the sessionId, personType, personId and klassId of the user which logged in.
+````
+{ 
+  sessionId: '39DA6ABB504V1ERA682S5C3FC3CCE4DF5',
+  personType: 5,
+  personId: 244,
+  klasseId: 19 
+}
+````
+
+## getTimetable
+#### Parameters
+* sessionId
+* personType
+* personId
+* startDate (optional)
+* endDate (optional)
+#### Usage
+````js
+let timetable = await getTimetable(sessionId, personType, personId)
+let timetable = await getTimetable(sessionId, personType, personId, startDate, endDate)
+````
+#### Returns
+a array of lessons which should looks like this:
+````
+[ { id: 89,
+    date: 20160509,
+    startTime: 1210,
+    endTime: 1300,
+    kl:
+     [ { id: 23,
+         name: '4A',
+         longname: 'Abt. Informationstechnologie' } ],
+    te: [ { id: 18, name: 'Bob' } ],
+    su: [ { id: 27, name: 'D', longname: 'DEUTSCH' } ],
+    ro: [ { id: 9, name: '367', longname: 'Klasse' } ],
+    activityType: 'Unterricht' },
+    .
+    .
+    .
+    ]
+````
+
+## getTeachers
+#### Parameters
+* sessionId
+#### Usage
+````js
+let teachers = await webuntis.getLatestImportTime(sessionId)
+````
+#### Returns
+a array of teachers which should looks like this:
+````
+[{ id: 12,
+    name: 'SMT',
+    foreName: 'Alex',
+    longName: 'Smith',
+    title: '',
+    active: true,
+    dids: [ [Object], [Object] ] },
+    .
+    .
+    .
+    ]
+````
+
+## getStudents
+#### Parameters
+* sessionId
+#### Usage
+````js
+let students = await webuntis.getStudents(sessionId)
+````
+#### Returns
+a array of students which should looks like this:
+````
+[{ id: 13,
+    key: '1164725150374',
+    name: 'smith.b01',
+    foreName: 'Bob',
+    longName: 'Smith',
+    gender: 'male' },
+    .
+    .
+    .
+    ]
+````
+
+## getSubjects
+#### Parameters
+* sessionId
+#### Usage
+````js
+let subjects = await webuntis.getSubjects(sessionId)
+````
+#### Returns
+a array of subjects which should looks like this:
+````
+[{ id: 251,
+    name: 'GGP',
+    longName: 'GEOGRAFIE, GESCHICHTE U. POL. BILD.',
+    alternateName: '',
+    active: true },
+    .
+    .
+    .
+    ]
+````
+
+## getRooms
+#### Parameters
+* sessionId
+#### Usage
+````js
+let rooms = await webuntis.getRooms(sessionId)
+````
+#### Returns
+a array of rooms which should looks like this:
+````
+[{ id: 66,
+    name: '364',
+    longName: 'Klasse',
+    active: true,
+    did: 8,
+    building: '' },
+    .
+    .
+    .
+    ]
+````
+
+## getDepartments
+#### Parameters
+* sessionId
+#### Usage
+````js
+let departments = await webuntis.getDepartments(sessionId)
+````
+#### Returns
+a array of departments which should looks like this:
+````
+[{ id: 5, name: 'E', longName: 'ELEKTROTECHNIK' },
+  { id: 2, name: 'IT', longName: 'INFORMATIONSTECHNOLOGIE' },
+    .
+    .
+    .
+    ]
+````
+
+## getHolidays
+#### Parameters
+* sessionId
+#### Usage
+````js
+let holidays = await webuntis.getHolidays(sessionId)
+````
+#### Returns
+a array of holidays which should looks like this:
+````
+[{ id: 7,
+    name: '26.10.',
+    longName: 'Nationalfeiertag',
+    startDate: 20181026,
+    endDate: 20181026 },
+    .
+    .
+    .
+    ]
+````
+
+## getTimeGrid
+#### Parameters
+* sessionId
+#### Usage
+````js
+let timeGrid = await webuntis.getTimeGrid(sessionId)
+````
+#### Returns
+a array of days with their timegrid which should looks like this:
+````
+[{ day: 2,
+  timeUnits:
+   [ { name: '1', startTime: 700, endTime: 750 },
+     { name: '2', startTime: 750, endTime: 840 },
+     { name: '3', startTime: 840, endTime: 930 },
+     { name: '4', startTime: 930, endTime: 1020 },
+     { name: '5', startTime: 1030, endTime: 1120 },
+     { name: '6', startTime: 1120, endTime: 1210 },
+    .
+    .
+    .
+    ]
+````
+
+## getCurrentSchoolYear
+#### Parameters
+* sessionId
+#### Usage
+````js
+let schoolyear = await webuntis.getCurrentSchoolYear(sessionId)
+````
+#### Returns
+a json object which should looks like this:
+````
+{ id: 1,
+  name: '2018/2019',
+  startDate: 20180903,
+  endDate: 20190630 }
+````
+
+## Logout
+#### Parameters
+* sessionId
+#### Usage
+````js
+webuntis.Logout(sessionId)
+````
+#### Returns
+a string which says if the logout was successful or not
+
+## Notice
+
+I am not affiliated with Untis GmbH. Use this at your own risk.
---
+++ node-webuntis.js
@@ -0,0 +1,435 @@
+
+///////////////////////////////////////////////////////////////////////////
+//////////////////////////// Dependencies /////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+const fetch = require('node-fetch'); 
+const colors = require('colors/safe') 
+const dateFormat = require('dateformat')
+
+var exports = module.exports = {}; // to make thing easier to export
+
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////// Code ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+//get the sessionId, personId and personType from Webuntis
+exports.getSession = async(schoolName, userName, password) => {
+
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do?school=${schoolName}`, {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id": "1",
+        "method": "authenticate",
+        "params": 
+            {"user": userName, "password": password, "client": "jsclient"}, 
+        "jsonrpc": "2.0"
+        })
+    })
+
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get timetable of current day 
+exports.getTimetable = async (sessionId, personType, personId, startDate, endDate) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getTimetable", 
+        "params": 
+            {
+            "options": 
+                { 
+                    "element": {"id": personId, "type": personType, "keyType": "id"},  
+                    "klasseFields": ["id", "name", "longname"],
+                    "roomFields": ["id", "name", "longname"],
+                    "subjectFields": ["id", "name", "longname"],
+                    "teacherFields": ["id", "name"],
+                    "startDate": startDate == undefined ? "" : startDate ,  // Format: yyyymmdd
+                    "endDate": endDate == undefined ? "" : endDate  // Format: yyyymmdd
+                }
+            },
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        if (json.result[0] !=  undefined) {
+            return await sortTimetable(json.result)
+        }
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get the timetable for the current week
+exports.getCurrentWeek = async (sessionId, personType, personId) => {
+    let monday = getMonday()
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getTimetable", 
+        "params": 
+            {
+            "options": 
+                { 
+                    "element": {"id": personId, "type": personType, "keyType": "id"},  
+                    "klasseFields": ["id", "name", "longname"],
+                    "roomFields": ["id", "name", "longname"],
+                    "subjectFields": ["id", "name", "longname"],
+                    "teacherFields": ["id", "name"],
+                    "startDate": dateFormat(new Date(monday), 'yyyymmdd'),
+                    "endDate": dateFormat(new Date(monday + 432000000), 'yyyymmdd')
+                }
+            },
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        if (json.result[0] !=  undefined) {
+            return await sortTimetable(json.result)
+        }
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get all teachers 
+exports.getTeachers = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getTeachers", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get all students 
+exports.getStudents = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getStudents", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get all (base) classes 
+exports.getClasses = async (sessionId, schollYearId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getKlassen", 
+        "params": {"schoolyearId": schollYearId},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get all subjects 
+exports.getSubjects = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getSubjects", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get all rooms 
+exports.getRooms = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getRooms", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get all departments 
+exports.getDepartments = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getDepartments", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get holidays 
+exports.getHolidays = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getHolidays", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get timeGrid 
+exports.getTimeGrid = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getTimegridUnits", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get statusData 
+exports.getStatusData = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getStatusData", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get currentSchollYear 
+exports.getCurrentSchoolYear = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getCurrentSchoolyear", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get schoolYears 
+exports.getSchoolYears = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getSchoolyears", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return json.result
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//get latestImportTime 
+exports.getLatestImportTime = async (sessionId) => {
+    let res = await fetch(`https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=${sessionId}?school=htl-ottakring`, { //post request to the WebUntis API
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"getLatestImportTime", 
+        "params": {},
+        "jsonrpc":"2.0",
+        })
+    })
+    
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        return dateFormat(new Date(json.result), 'HH:MM yyyymmdd')
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+}
+
+//Loging out 
+exports.Logout = async(sessionID) => {
+    let res =  await fetch("https://melpomene.webuntis.com/WebUntis/jsonrpc.do;jsessionid=" + sessionID + "?school=htl-ottakring", {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        "id":"ID",
+        "method":"logout",
+        "params":{},
+        "jsonrpc":"2.0"
+        })
+    })
+       
+    let json = await res.json();
+
+    if(json.error == undefined){ 
+        console.log(colors.green("Successfully logged out"))
+    }else{
+        throw new Error(colors.red(json.error.message)) 
+    }
+
+}
+
+//gets the first day of the week
+getMonday = () => {
+let time = Date.now();
+    let dayOfWeek = dateFormat(new Date(), "N");
+    for (let i = 0; i < dayOfWeek-1; i++) {
+        time -= 86400000;        
+    }
+
+    //console.log(dateFormat(new Date(time), "yyyymmdd"));
+    return time;
+}
+
+sortTimetable = async (rawTimetable) => {
+    
+    let timetable = []
+
+    rawTimetable.sort((a, b) => { return a.date - b.date;}); //sorts the WebUntis data after the date
+    
+    let firstDay =  new Date(`${rawTimetable[0].date.toString().slice(0, 4)}-${rawTimetable[0].date.toString().slice(4, 6)}-${rawTimetable[0].date.toString().slice(6, 8)}`).getTime();
+    let lastDay =  new Date(`${rawTimetable[rawTimetable.length - 1].date.toString().slice(0, 4)}-${rawTimetable[rawTimetable.length - 1].date.toString().slice(4, 6)}-${rawTimetable[rawTimetable.length - 1].date.toString().slice(6, 8)}`).getTime();
+    
+    let daysCount = Math.ceil(Math.abs(lastDay - firstDay) / (1000 * 3600 * 24)) + 1
+
+    for (let i = 0; i < daysCount; i++) {
+        let date = dateFormat(new Date(firstDay), 'yyyymmdd')
+        let day = rawTimetable.filter(lesson => lesson.date == date)
+        
+        day.sort((a, b) => { return a.startTime - b.startTime})
+        
+        timetable[i] = day
+        firstDay += 86400000
+    }
+
+    return Promise.resolve(timetable);
+}
